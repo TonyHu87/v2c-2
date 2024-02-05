efdeb56567d258cf77f15268ef9b5470ad5065e7
TonyHu87 <pahc2008@gmail.com>
2024-02-05 18:00:33 +0800

    tonyhu87

--- src/init.sh
+++ src/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-author=233boy copy
-# github=https://github.com/233boy/v2ray
+author=TonyHu87 copy
+# github=https://github.com/TonyHu87/v2ray
 
 # bash fonts colors
 red='\e[31m'
@@ -133,7 +133,7 @@ _wget() {
 
 load core.sh
 # old sh ver
-is_old_dir=/etc/v2ray/233boy
+is_old_dir=/etc/v2ray/TonyHu87
 is_old_conf=/etc/v2ray/233blog_v2ray_backup.conf
 if [[ -f $is_old_conf && -d $is_old_dir ]]; then
     load old.sh
